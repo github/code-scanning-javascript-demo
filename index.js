a100f03e03a4bc6ba4fecb9bd6faa0a78477783d
Paromita Roy <55242088+paromitaroy@users.noreply.github.com>
2022-06-10 12:24:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -261,8 +261,7 @@ exports.extract = function (cwd, opts) {
     var onlink = function () {
       if (win32) return next() // skip links on win for now before it can be tested
       xfs.unlink(name, function () {
-       // var srcpath = path.resolve(cwd, header.linkname)
-        var srcpath = path.join(cwd, path.join('/', header.linkname))
+        var srcpath = path.resolve(cwd, header.linkname)
         
 
         xfs.link(srcpath, name, function (err) {
