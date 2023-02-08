53dc329b09f2360ab61917f89e84c7296251a5ad
Marco Celon <20218924+nolecram@users.noreply.github.com>
2023-02-08 13:00:05 +0000

    BubuGaga

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 Hello Hello....
 Let's try some functionalities
+
+Problem: var srcpath = path.resolve(cwd, header.linkname)
+Solution: var srcpath = path.join(cwd, path.join('/', header.linkname))
--- fail.js
+++
@@ -1,4 +0,0 @@
-    var onlink = function () {
-      if (win32) return next() // skip links on win for now before it can be tested
-      xfs.unlink(name, function () {
-        var srcpath = path.resolve(cwd, header.linkname)
